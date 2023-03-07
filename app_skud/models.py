662ab6288fed70d874525404e2210fbec040cd39
RomanL91 <dev_roman91@mail.ru>
2023-03-07 11:20:36 +0600

    returned entity(MonitorCheckAccess) to preserve imports

--- app_skud/models.py
+++ app_skud/models.py
@@ -81,4 +81,11 @@ class MonitorEvents(models.Model):
 
     
 class MonitorCheckAccess(models.Model):
-    pass
+    staff = models.ForeignKey(Staffs, on_delete=models.SET_NULL, null=True, blank=True)
+    controller = models.ForeignKey('app_controller.Controller', on_delete=models.SET_NULL, null=True, blank=True)
+    data_monitor = models.JSONField(editable=False, verbose_name='хранилище экземпляра', default=dict)
+
+    def __str__(self) -> str:
+        if self.staff is None:
+            return f'{self.staff}'
+        return self.staff.last_name
