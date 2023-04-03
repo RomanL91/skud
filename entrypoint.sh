605e7ccdd52b99da77cc6e4890abe2190712bffa
RomanL91 <dev_roman91@mail.ru>
2023-04-03 17:14:50 +0600

    the best implementation

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py migrate
 
 python manage.py collectstatic --no-input
 
-# gunicorn --bind 0.0.0.0:8000 myproject.asgi -w 4 -k uvicorn.workers.UvicornWorker
+# gunicorn --bind 0.0.0.0:8000 core.asgi -w 4 -k uvicorn.workers.UvicornWorker
 uvicorn core.asgi:application --host 0.0.0.0 --port 8000
