9c3b547da7c2063752e40743e838bc908cd4315a
RomanL91 <dev_roman91@mail.ru>
2023-07-11 15:58:08 +0600

    изменено имя приложения

--- app_time_profile/apps.py
+++ app_time_profile/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppTimeProfileConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_time_profile"
+    verbose_name = "Профиля доступа по времени"
