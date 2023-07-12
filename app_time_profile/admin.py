07fd6da825fe09bb9f54ead33dce666872f81abc
RomanL91 <dev_roman91@mail.ru>
2023-07-12 12:07:55 +0600

    Создана сущность профиля времени

--- app_time_profile/admin.py
+++ app_time_profile/admin.py
@@ -5,14 +5,20 @@ from app_time_profile.models import TimeProfile
 
 @admin.register(TimeProfile)
 class TimeProfileAdmin(admin.ModelAdmin):
-    fields = [
-        ('name_time_profile',
-        'descriptions',),
-        'monday',
-        'tuesday',
-        'wednesday',
-        'thursday',
-        'friday',
-        'saturday',
-        'sunday',
-    ]
+    fieldsets = (
+        (None, {'fields': ('name_time_profile', 'descriptions')}),
+        ('Понедельник', {'fields': (('monday_time_stert', 'monday_time_end'),), 'classes':('collapse',)}),
+        ('Вторник', {'fields': (('tuesday_time_start', 'tuesday_time_end'),), 'classes':('collapse',)}),
+        ('Среда', {'fields': (('wednesday_time_start', 'wednesday_time_end'),), 'classes':('collapse',)}),
+        ('Четверг', {'fields': (('thursday_time_start', 'thursday_time_end'),), 'classes':('collapse',)}),
+        ('Пятница', {'fields': (('friday_time_start', 'friday_time_end'),), 'classes':('collapse',)}),
+        ('Суббота', {'fields': (('saturday_time_start', 'saturday_time_end'),), 'classes':('collapse',)}),
+        ('Воскресенье', {'fields': (('sunday_time_start', 'sunday_time_end'),), 'classes':('collapse',)}),
+    )
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['show_save'] = True
+        extra_context['show_save_and_continue'] = False
+        extra_context['show_save_and_add_another'] = False
+        return super().changeform_view(request, object_id, form_url, extra_context)
--- app_time_profile/models.py
+++ app_time_profile/models.py
@@ -1,18 +1,46 @@
+import datetime
+
 from django.db import models
 
 
 help_text = 'Пожалуйста, используте установленный формат: ЧЧ:ММ'
+ver_name_start_d = 'начало рабочего дня'
+ver_name_end_d = 'конец рабочего дня'
+
+
+def get_time_choices(start_time=datetime.time(0,0,0), end_time=datetime.time(23,0,0), delta=datetime.timedelta(minutes=15)):
+    time_choices = ()
+    time = start_time
+    while time <= end_time:
+        time_choices += ((time, time.strftime("%H:%M")),)
+        time = (datetime.datetime.combine(datetime.date.today(), time) + delta).time()
+    return time_choices
 
 class TimeProfile(models.Model):
     name_time_profile = models.CharField(verbose_name='Название профиля доступа', unique=True, max_length= 150, help_text='Поле для ввода названия профиля доступа по времени')
     descriptions = models.TextField(verbose_name='Описание профиля доступа', blank=True, help_text='Поле для ввода описания данного профиля доступа по времени')
-    monday =  models.TimeField(verbose_name='Понедельник', blank=True, null=True, help_text=help_text)
-    tuesday =  models.TimeField(verbose_name='Вторник', blank=True, null=True, help_text=help_text)
-    wednesday = models.TimeField(verbose_name='Среда', blank=True, null=True, help_text=help_text)
-    thursday = models.TimeField(verbose_name='Четверг', blank=True, null=True, help_text=help_text)
-    friday = models.TimeField(verbose_name='Пятница', blank=True, null=True, help_text=help_text)
-    saturday = models.TimeField(verbose_name='Суббота', blank=True, null=True, help_text=help_text)
-    sunday = models.TimeField(verbose_name='Воскресенье', blank=True, null=True, help_text=help_text)
+    
+    monday_time_stert =  models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    monday_time_end =  models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+    
+    tuesday_time_start =  models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    tuesday_time_end =  models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+    
+    wednesday_time_start = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    wednesday_time_end = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+    
+    thursday_time_start = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    thursday_time_end = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+    
+    friday_time_start = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    friday_time_end = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+    
+    saturday_time_start = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    saturday_time_end = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+    
+    sunday_time_start = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_start_d, blank=True, null=True, help_text=help_text)
+    sunday_time_end = models.TimeField(choices=get_time_choices(), verbose_name=ver_name_end_d, blank=True, null=True, help_text=help_text)
+
     time_profile_data =  models.JSONField(verbose_name='Хранилище экземпляра', blank=True, default=dict, editable=False)
 
     def __str__(self) -> str:
