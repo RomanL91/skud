a224bef0b71fde616f635fe46100f811e7c94685
RomanL91 <dev_roman91@mail.ru>
2023-10-03 12:13:33 +0600

    удаление не используемого

--- app_skud/urls.py
+++ app_skud/urls.py
@@ -1,16 +1,5 @@
-from django.urls import path, re_path
-
-from app_skud.view import MonitorEventsListView, CheckpointsListView, CheckpointDetailView
-
 from django.contrib import admin
 
 admin.site.site_header = 'Система Контроля и Управления Доступом'
 admin.site.index_title = ''                 # default: "Site administration"
 admin.site.site_title = 'СКУД'  
-
-
-urlpatterns = [
-    # path("", MonitorEventsListView.as_view(), name="monitor_events"),
-    # path("selects_monitors/", CheckpointsListView.as_view(), name="selects_monitors"),
-    # re_path(r'^selects_monitors/(?P<pk>\d+)$', CheckpointDetailView.as_view(), name="monitors_detail_checkpoint"),
-]
