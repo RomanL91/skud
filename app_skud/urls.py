cb499f7754600508e660bcb5496e8d53def1bdae
RomanL91 <dev_roman91@mail.ru>
2023-10-04 10:08:10 +0600

    версии python

--- app_skud/urls.py
+++ app_skud/urls.py
@@ -3,3 +3,10 @@ from django.contrib import admin
 admin.site.site_header = 'Система Контроля и Управления Доступом'
 admin.site.index_title = ''                 # default: "Site administration"
 admin.site.site_title = 'СКУД'  
+
+
+urlpatterns = [
+    # path("", MonitorEventsListView.as_view(), name="monitor_events"),
+    # path("selects_monitors/", CheckpointsListView.as_view(), name="selects_monitors"),
+    # re_path(r'^selects_monitors/(?P<pk>\d+)$', CheckpointDetailView.as_view(), name="monitors_detail_checkpoint"),
+]
