116b0819430269d211977584ab48603d064c5799
RomanL91 <dev_roman91@mail.ru>
2023-10-31 13:17:18 +0600

    list dysplay in observer

--- app_observer/admin.py
+++ app_observer/admin.py
@@ -5,4 +5,15 @@ from app_observer.models import PerimeterMonitor
 
 @admin.register(PerimeterMonitor)
 class PerimeterMonitorAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        'name',
+        'description',
+        # 'perimeter_gates',
+        'perimeter_counter',
+        'get_perimeter_gates',
+        # 'perimeter_data',
+    ]
+
+    def get_perimeter_gates(self, obj):
+        return " | ".join([checkpoint.name_checkpoint for checkpoint in obj.perimeter_gates.all()])
+    get_perimeter_gates.short_description = 'Проходные'
